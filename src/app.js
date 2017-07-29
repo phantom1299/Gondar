ee9c684e21e9ea559d631b0ab5b0df0e785d0922
mlx <muhammed.1299x@gmail.com>
2017-07-29 22:55:39 +0300

    Hot reloading özelleğini Redux ile kullanmak için düzenlemeler yapıldı

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
 import Router from './Router';
+import configureStore from './configureStore';
 
 console.ignoredYellowBox = [
     'Setting a timer'
 ];
 
 class App extends Component {
+    state= {
+        store: configureStore()
+    }
 
     componentWillMount() {
         const config = {
@@ -26,10 +27,8 @@ class App extends Component {
     }
 
     render() {
-        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-
         return (
-            <Provider store={store}>
+            <Provider store={this.state.store}>
                 <Router />
             </Provider>
         );
